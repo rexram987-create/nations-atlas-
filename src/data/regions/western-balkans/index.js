567f094d48b9dc16ec4b1da495b9e2b54b422cf1
rexram987-create <rexram987@gmail.com>
2026-04-30 08:11:31 +0300

    Add Western Balkans data index

---
+++ src/data/regions/western-balkans/index.js
@@ -0,0 +1,19 @@
+import serbia from "./serbia.js";
+import croatia from "./croatia.js";
+import bosniaAndHerzegovina from "./bosnia-and-herzegovina.js";
+import slovenia from "./slovenia.js";
+import montenegro from "./montenegro.js";
+import kosovo from "./kosovo.js";
+import northMacedonia from "./north-macedonia.js";
+
+const westernBalkans = [
+  serbia,
+  croatia,
+  bosniaAndHerzegovina,
+  slovenia,
+  montenegro,
+  kosovo,
+  northMacedonia
+];
+
+export default westernBalkans;
