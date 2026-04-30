d0df8865442bee49cd83bf341176f2eebc956554
rexram987-create <rexram987@gmail.com>
2026-04-30 14:08:56 +0300

    Add Eastern and Southern Balkans data index

---
+++ src/data/regions/eastern-southern-balkans/index.js
@@ -0,0 +1,15 @@
+import romania from "./romania.js";
+import bulgaria from "./bulgaria.js";
+import greece from "./greece.js";
+import albania from "./albania.js";
+import northMacedonia from "../western-balkans/north-macedonia.js";
+
+const easternSouthernBalkans = [
+  romania,
+  bulgaria,
+  greece,
+  albania,
+  northMacedonia
+];
+
+export default easternSouthernBalkans;
