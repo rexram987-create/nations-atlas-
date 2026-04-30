7343d6956f733b7c9d242639c60464faf3b24881
rexram987-create <rexram987@gmail.com>
2026-04-30 08:07:54 +0300

    Add Bosnia and Herzegovina country data file

---
+++ src/data/regions/western-balkans/bosnia-and-herzegovina.js
@@ -0,0 +1,34 @@
+const bosniaAndHerzegovina = {
+  id: "bosnia-and-herzegovina",
+  region: "western-balkans",
+  name: {
+    he: "בוסניה והרצגובינה",
+    en: "Bosnia and Herzegovina"
+  },
+  englishName: "Bosnia and Herzegovina",
+  officialName: {
+    he: "בוסניה והרצגובינה",
+    en: "Bosnia and Herzegovina"
+  },
+  flag: "https://flagcdn.com/w640/ba.webp",
+  capital: {
+    name: {
+      he: "סרייבו",
+      en: "Sarajevo"
+    },
+    englishName: "Sarajevo"
+  },
+  population: "~3,200,000",
+  area: {
+    he: "~51,209 קמ״ר",
+    en: "~51,209 sq km"
+  },
+  currency: {
+    he: "מארק בר-המרה (BAM)",
+    en: "Convertible mark (BAM)"
+  },
+  sourceHtml: "regions/western_balkans.html",
+  migrationStatus: "basic-fields-extracted"
+};
+
+export default bosniaAndHerzegovina;
