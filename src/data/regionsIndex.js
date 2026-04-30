ba281f2d044c553aedd0dd1945aca177a7afaed6
rexram987-create <rexram987@gmail.com>
2026-04-30 07:18:28 +0300

    Add central regions data index

---
+++ src/data/regionsIndex.js
@@ -0,0 +1,14 @@
+import balticStates from "./regions/baltic-states/index.js";
+
+const regionsIndex = {
+  "baltic-states": {
+    id: "baltic-states",
+    name: {
+      he: "המדינות הבלטיות",
+      en: "Baltic States"
+    },
+    countries: balticStates
+  }
+};
+
+export default regionsIndex;
