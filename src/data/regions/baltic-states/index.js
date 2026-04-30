b62f317a6af6e1d2ffdbe65fe12bba910ddd2322
rexram987-create <rexram987@gmail.com>
2026-04-30 07:18:12 +0300

    Add Baltic States data index

---
+++ src/data/regions/baltic-states/index.js
@@ -0,0 +1,11 @@
+import estonia from "./estonia.js";
+import latvia from "./latvia.js";
+import lithuania from "./lithuania.js";
+
+const balticStates = [
+  estonia,
+  latvia,
+  lithuania
+];
+
+export default balticStates;
