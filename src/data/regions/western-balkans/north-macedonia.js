fb3d9fb6afd8d50b3dcd403d5f957a5d67242231
rexram987-create <rexram987@gmail.com>
2026-04-30 08:11:12 +0300

    Add North Macedonia country data file

---
+++ src/data/regions/western-balkans/north-macedonia.js
@@ -0,0 +1,34 @@
+const northMacedonia = {
+  id: "north-macedonia",
+  region: "western-balkans",
+  name: {
+    he: "צפון מקדוניה",
+    en: "North Macedonia"
+  },
+  englishName: "North Macedonia",
+  officialName: {
+    he: "רפובליקת צפון מקדוניה",
+    en: "Republic of North Macedonia"
+  },
+  flag: "https://flagcdn.com/w640/mk.webp",
+  capital: {
+    name: {
+      he: "סקופיה",
+      en: "Skopje"
+    },
+    englishName: "Skopje"
+  },
+  population: "~1,800,000",
+  area: {
+    he: "~25,713 קמ״ר",
+    en: "~25,713 sq km"
+  },
+  currency: {
+    he: "דנר מקדוני (MKD)",
+    en: "Macedonian denar (MKD)"
+  },
+  sourceHtml: "regions/western_balkans.html",
+  migrationStatus: "basic-fields-extracted"
+};
+
+export default northMacedonia;
