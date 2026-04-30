83315a9d4462b4846448d8f6e6daa19cfea60201
rexram987-create <rexram987@gmail.com>
2026-04-30 07:17:38 +0300

    Add Latvia starter data file

---
+++ src/data/regions/baltic-states/latvia.js
@@ -0,0 +1,16 @@
+const latvia = {
+  id: "latvia",
+  region: "baltic-states",
+  name: {
+    he: "לטביה",
+    en: "Latvia"
+  },
+  capital: {
+    he: "ריגה",
+    en: "Riga"
+  },
+  sourceHtml: "regions/baltic_states.html",
+  migrationStatus: "starter-file"
+};
+
+export default latvia;
