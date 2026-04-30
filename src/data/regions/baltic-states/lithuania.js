870832159e75430121050bc2c89d2a467a64deda
rexram987-create <rexram987@gmail.com>
2026-04-30 07:17:51 +0300

    Add Lithuania starter data file

---
+++ src/data/regions/baltic-states/lithuania.js
@@ -0,0 +1,16 @@
+const lithuania = {
+  id: "lithuania",
+  region: "baltic-states",
+  name: {
+    he: "ליטא",
+    en: "Lithuania"
+  },
+  capital: {
+    he: "וילנה",
+    en: "Vilnius"
+  },
+  sourceHtml: "regions/baltic_states.html",
+  migrationStatus: "starter-file"
+};
+
+export default lithuania;
