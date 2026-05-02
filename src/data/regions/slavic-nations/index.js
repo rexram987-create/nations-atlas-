1101fae9ff35da6a77da6c9bf3855251b2189d24
rexram987-create <rexram987@gmail.com>
2026-05-02 16:19:03 +0300

    Add Slavic Nations data index

---
+++ src/data/regions/slavic-nations/index.js
@@ -0,0 +1,17 @@
+import russia from "./russia.js";
+import ukraine from "./ukraine.js";
+import poland from "./poland.js";
+import czechia from "./czechia.js";
+import slovakia from "./slovakia.js";
+import belarus from "./belarus.js";
+
+const slavicNations = [
+  russia,
+  ukraine,
+  poland,
+  czechia,
+  slovakia,
+  belarus
+];
+
+export default slavicNations;
