5df6226f667290fb335b6fe3ac4f73530c9ff7d1
rexram987-create <rexram987@gmail.com>
2026-05-03 18:06:06 +0300

    Add Stan Nations data index

---
+++ src/data/regions/stan-nations/index.js
@@ -0,0 +1,19 @@
+import afghanistan from "./afghanistan.js";
+import kazakhstan from "./kazakhstan.js";
+import uzbekistan from "./uzbekistan.js";
+import turkmenistan from "./turkmenistan.js";
+import kyrgyzstan from "./kyrgyzstan.js";
+import tajikistan from "./tajikistan.js";
+import pakistan from "./pakistan.js";
+
+const stanNations = [
+  afghanistan,
+  kazakhstan,
+  uzbekistan,
+  turkmenistan,
+  kyrgyzstan,
+  tajikistan,
+  pakistan
+];
+
+export default stanNations;
