7ecca2e57b05015846522be69b493ee24642656b
rexram987-create <rexram987@gmail.com>
2026-05-06 19:41:25 +0300

    Add Middle East countries data index

---
+++ data/middle-east/index.js
@@ -0,0 +1,25 @@
+import egypt from './egypt.js';
+import israel from './israel.js';
+import jordan from './jordan.js';
+import lebanon from './lebanon.js';
+import syria from './syria.js';
+import iraq from './iraq.js';
+
+const middleEastCountries = [
+  egypt,
+  israel,
+  jordan,
+  lebanon,
+  syria,
+  iraq
+];
+
+export default middleEastCountries;
+export {
+  egypt,
+  israel,
+  jordan,
+  lebanon,
+  syria,
+  iraq
+};
